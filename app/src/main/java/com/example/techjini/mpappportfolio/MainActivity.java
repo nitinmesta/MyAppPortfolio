9c6f8203652fc22b32b3e1cab557f6dca144941f
Nitin Mesta <nitin@techjini.com>
2016-01-26 13:04:37 +0530

    Created the required layout and the set the listener for the button to show the message on click

--- app/src/main/java/com/example/techjini/mpappportfolio/MainActivity.java
+++ app/src/main/java/com/example/techjini/mpappportfolio/MainActivity.java
@@ -8,8 +8,10 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,7 +19,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
+        findViewById(R.id.btn_spotify_streamer).setOnClickListener(this);
+        findViewById(R.id.btn_scores_app).setOnClickListener(this);
+        findViewById(R.id.btn_spotify_streamer).setOnClickListener(this);
+        findViewById(R.id.btn_library_app).setOnClickListener(this);
+        findViewById(R.id.btn_build_it_bigger_app).setOnClickListener(this);
+        findViewById(R.id.btn_xyz_reader).setOnClickListener(this);
+        findViewById(R.id.btn_capstone_my_own_app).setOnClickListener(this);
 
     }
 
@@ -28,18 +36,37 @@ public class MainActivity extends AppCompatActivity {
         return true;
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+
+    @Override
+    public void onClick(View v) {
+
+        switch (v.getId())
+        {
+            case R.id.btn_spotify_streamer:
+                showToast(getString(R.string.spotify_streamer));
+                break;
+            case R.id.btn_scores_app:
+                showToast(getString(R.string.scores_app));
+                break;
+            case R.id.btn_library_app:
+                showToast(getString(R.string.library_app));
+                break;
+            case R.id.btn_build_it_bigger_app:
+                showToast(getString(R.string.build_it_bigger));
+                break;
+            case R.id.btn_xyz_reader:
+                showToast(getString(R.string.xyz_reader));
+                break;
+            case R.id.btn_capstone_my_own_app:
+                showToast(getString(R.string.capstone_my_own_app));
+                break;
         }
 
-        return super.onOptionsItemSelected(item);
+    }
+
+    private void showToast(String message)
+    {
+        Toast.makeText(this,String.format("This button will launch %s",message),Toast.LENGTH_LONG).show();
     }
 }
